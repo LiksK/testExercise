0c91e8705958269bfa8db167815ba93a71823117
LiksK <144459112+LiksK@users.noreply.github.com>
2024-12-01 11:59:06 +0300

    Add files via upload

---
+++ testExersice_V1/script.js
@@ -0,0 +1,19 @@
+let btnMenu = document.getElementById("btn-menu")
+let menu = document.getElementById("menuNav")
+let flag = 0;
+
+btnMenu.onclick = function(){
+    if (flag) {
+        menu.innerHTML = ``
+        flag = 0
+    }
+    else{
+        menu.innerHTML = `<ul class="nav-list">
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Профпробы</a></li>
+                <li><a href="#">Партнеры.Экскурсии</a></li>
+            </ul>`
+        flag = 1
+    }
+    
+}
